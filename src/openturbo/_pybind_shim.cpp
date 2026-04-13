397e1255c8fefce1ceabd4ccfa784ad004816494
TORlN <nelsontorin1@gmail.com>
2026-04-13 02:16:08 -0600

    Add CUDA launch function declarations for encoding and scanning operations in pybind shim

--- src/openturbo/_pybind_shim.cpp
+++ src/openturbo/_pybind_shim.cpp
@@ -1,6 +1,36 @@
-#include "../../kernels/openturbo_cuda_api.cuh"
-
 #include <cuda_runtime.h>
+
+#ifdef __INTELLISENSE__
+#include "../../kernels/encoder_layout.cuh"
+
+namespace openturbo
+{
+    cudaError_t launch_encode_tile_fused(
+        const float *input,
+        PackedTileHeader *output_headers,
+        int num_tiles,
+        int token_pos,
+        float rope_theta,
+        cudaStream_t stream = nullptr);
+
+    cudaError_t launch_scan_query_many_cache(
+        const PackedTileHeader *query_header,
+        const PackedTileHeader *cache_headers,
+        float *output,
+        int num_cache_tiles,
+        cudaStream_t stream = nullptr);
+
+    cudaError_t launch_scan_query_many_cache_multi_tile(
+        const PackedTileHeader *query_headers,
+        const PackedTileHeader *cache_headers,
+        float *output,
+        int num_query_tiles,
+        int num_cache_tokens,
+        cudaStream_t stream = nullptr);
+}
+#else
+#include "../../kernels/openturbo_cuda_api.cuh"
+#endif
 
 #include <cstdint>
 #include <stdexcept>
